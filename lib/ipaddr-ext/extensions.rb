7c61579f604445c1d74fa2023a8d68479ebd7043
Taketo Takashima <t.taketo1113@gmail.com>
2022-12-02 23:44:36 +0900

    Added wildcard_mask method

--- lib/ipaddr-ext/extensions.rb
+++ lib/ipaddr-ext/extensions.rb
@@ -4,5 +4,19 @@ require_relative "version"
 
 module IPAddrExt
   module Extensions
+    # Returns the wildcard mask in string format e.g. 0.0.255.255
+    # ref: https://github.com/ruby/ipaddr/pull/44
+    def wildcard_mask
+      case @family
+      when Socket::AF_INET
+        mask = IPAddr::IN4MASK ^ @mask_addr
+      when Socket::AF_INET6
+        mask = IPAddr::IN6MASK ^ @mask_addr
+      else
+        raise AddressFamilyError, "unsupported address family"
+      end
+
+      _to_string(mask)
+    end
   end
 end
--- spec/ipaddr-ext_spec.rb
+++ spec/ipaddr-ext_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe IPAddrExt do
   it "has a version number" do
     expect(IPAddrExt::VERSION).not_to be nil
   end
+
+  context "#wildcard_mask" do
+    it "ipv4" do
+      expect(IPAddr.new("192.168.1.2/1").wildcard_mask).to eq "127.255.255.255"
+      expect(IPAddr.new("192.168.1.2/8").wildcard_mask).to eq "0.255.255.255"
+      expect(IPAddr.new("192.168.1.2/16").wildcard_mask).to eq "0.0.255.255"
+      expect(IPAddr.new("192.168.1.2/24").wildcard_mask).to eq "0.0.0.255"
+      expect(IPAddr.new("192.168.1.2/32").wildcard_mask).to eq "0.0.0.0"
+    end
+
+    it "ipv6" do
+      expect(IPAddr.new("3ffe:505:2::/48").wildcard_mask).to eq "0000:0000:0000:ffff:ffff:ffff:ffff:ffff"
+      expect(IPAddr.new("3ffe:505:2::/128").wildcard_mask).to eq "0000:0000:0000:0000:0000:0000:0000:0000"
+    end
+  end
 end
